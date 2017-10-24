2deb861f9f966c06c47e469327af96135ff11ed4
Michael <Use Website Contact Please>
2017-10-23 23:37:09 -0500

    for diffing b

--- filter_offset.php
+++ filter_offset.php
@@ -5,7 +5,6 @@
 
 // load image
 $image1 = imagecreatefromjpeg("lighthouse.jpg");
-// $image1 = imagecreatefromjpeg("http://localhost/lighthouse.jpg");
 
 // get image dimensions for creating the final output canvas
 $dx = imagesx($image1);
@@ -30,8 +29,27 @@ function filterImage(&$imageInput, &$resultImage) {
 		for ($x=0; $x < $dx; $x++) {
 			//// formula goes here!
 
-			$sampleX = $x;
-			$sampleY = $y;
+			$offsetX = -90;
+			$offsetY = 60;
+
+			$sampleX = $x + $offsetX;
+			$sampleY = $y + $offsetY;
+
+			while ($sampleX >= $dx) {
+				$sampleX -= $dx;
+			}
+
+			while ($sampleX < 0) {
+				$sampleX += $dx;
+			}
+
+			while ($sampleY >= $dy) {
+				$sampleY -= $dy;
+			}
+
+			while ($sampleY < 0) {
+				$sampleY += $dy;
+			}
 
 			$rgbColor = imagecolorat($imageInput, $sampleX, $sampleY);
 			$r = ($rgbColor >> 16) & 0xFF; //getting other color values
@@ -40,13 +58,9 @@ function filterImage(&$imageInput, &$resultImage) {
 			imagecolordeallocate($imageInput, $rgbColor);
 
 
-
-			$rOut = $r;
-			$gOut = $g;
-			$bOut = $b;
 			//// forumla ends here!
 
-			$theColor = imagecolorallocate($resultImage, $rOut, $gOut, $bOut);
+			$theColor = imagecolorallocate($resultImage, $r, $g, $b);
 			imagesetpixel($resultImage, round($x),round($y), $theColor);
 			imagecolordeallocate($resultImage, $theColor);
 		}
