6a94b21c40af4976a85b4473cc42b1fa41e76f82
Michael <Use Website Contact Please>
2017-10-23 23:47:27 -0500

    for diffing b

--- filter_gaussian.php
+++ filter_gaussian.php
@@ -5,7 +5,6 @@
 
 // load image
 $image1 = imagecreatefromjpeg("lighthouse.jpg");
-// $image1 = imagecreatefromjpeg("http://localhost/lighthouse.jpg");
 
 // get image dimensions for creating the final output canvas
 $dx = imagesx($image1);
@@ -14,7 +13,7 @@ $dy = imagesy($image1);
 $result = imagecreatetruecolor($dx, $dy);
 
 // run the filter
-filterImage($image1, $result);
+filterImage($image1, $result, 7);
 
 // send results to client
 header('Content-Type: image/jpeg');
@@ -22,37 +21,142 @@ imagejpeg($result);
 imagedestroy($result);
 imagedestroy($image1);
 
-function filterImage(&$imageInput, &$resultImage) {
+
+// sourced from http://blog.ivank.net/fastest-gaussian-blur.html
+
+function filterImage(&$imageInput, &$resultImage, $blurAmount = 5) {
 	$dx = imagesx($imageInput);
 	$dy = imagesy($imageInput);
 
-	for ($y=0; $y < $dy; $y++) {
-		for ($x=0; $x < $dx; $x++) {
-			//// formula goes here!
 
-			$sampleX = $x;
-			$sampleY = $y;
+	// load entire image into fast arrays in memory
+	$rSource = array();
+	$gSource = array();
+	$bSource = array();
+	for ($yValue=0; $yValue < $dy; $yValue++) {
+		for ($xValue=0; $xValue < $dx; $xValue++) {
 
-			$rgbColor = imagecolorat($imageInput, $sampleX, $sampleY);
+			$rgbColor = imagecolorat($imageInput, $xValue, $yValue);
 			$r = ($rgbColor >> 16) & 0xFF; //getting other color values
 			$g = ($rgbColor >> 8) & 0xFF;
 			$b = $rgbColor & 0xFF;
 			imagecolordeallocate($imageInput, $rgbColor);
 
+			$rSource[] = $r;
+			$gSource[] = $g;
+			$bSource[] = $b;
+		}
+	}
 
 
-			$rOut = $r;
-			$gOut = $g;
-			$bOut = $b;
-			//// forumla ends here!
+	$rTarget = array();
+	$gTarget = array();
+	$bTarget = array();
 
-			$theColor = imagecolorallocate($resultImage, $rOut, $gOut, $bOut);
-			imagesetpixel($resultImage, round($x),round($y), $theColor);
-			imagecolordeallocate($resultImage, $theColor);
+	boxBlur($rSource, $rTarget, $dx, $dy, $blurAmount);
+	boxBlur($gSource, $gTarget, $dx, $dy, $blurAmount);
+	boxBlur($bSource, $bTarget, $dx, $dy, $blurAmount);
 
+
+	for ($i=0; $i < count($rTarget); $i++) {
+		$x = $i % $dx;
+		$y = intval($i / $dx);
+
+		$theColor = imagecolorallocate($resultImage, $rTarget[$i], $gTarget[$i], $bTarget[$i]);
+		imagesetpixel($resultImage, round($x),round($y), $theColor);
+		imagecolordeallocate($resultImage, $theColor);
+	}
+
+
+
+}
+
+
+function boxBlur(&$sourceChannelArray, &$targetChannelArray, $width, $height, $boxRadius) {
+	for ($i=0; $i < count($sourceChannelArray); $i++) {
+		$targetChannelArray[$i] = $sourceChannelArray[$i];
+	}
+	boxBlurH($targetChannelArray, $sourceChannelArray, $width, $height, $boxRadius);
+	boxBlurV($sourceChannelArray, $targetChannelArray, $width, $height, $boxRadius);
+}
+
+function boxBlurH(&$sourceChannelArray, &$targetChannelArray, $width, $height, $boxRadius) {
+
+	$accumlatorAverager = 1 / ($boxRadius + $boxRadius + 1); // radius range on either side of a pixel + the pixel itself
+	for ($i=0; $i < $height; $i++) {
+		$leadingIndex = $i * $width; //leading pixel index; will traverse the width of the image for each loop of the parent "for loop"
+		$trailingIndex = $leadingIndex; // trailing pixel index
+		$radiusIndex = $leadingIndex + $boxRadius; //pixel index of the furthest reach of the radius
+
+		$firstValue = $sourceChannelArray[$leadingIndex]; // first pixel value of the row
+		$lastValue = $sourceChannelArray[$leadingIndex + $width - 1]; // last pixel value in the row
+		$val = ($boxRadius + 1) * $firstValue; // create a "value accumulator" - we will be calculating the average of pixels surrounding each one - is faster to add newest value, remove oldest, and then average. This initial value is for pixels outside image bounds
+
+		//for length of radius, accumulate the total value of all pixels from current pixel index and record it into the target channel first pixel
+		for ($j = 0; $j < $boxRadius; $j++) {
+			$val += $sourceChannelArray[$leadingIndex + $j];
+		}
+
+		// for the next $boxRadius pixels in the row, record pixel value of average of all pixels within the radius and save average into target channel
+		for ($j = 0; $j <= $boxRadius; $j++) {
+			$val += $sourceChannelArray[$radiusIndex++] - $firstValue;
+			$targetChannelArray[$leadingIndex++] = round($val * $accumlatorAverager);
+		}
+
+		// now that we've completely removed the overflow pixels from the value accumulator, continue on, adding new values, removing old ones, and averaging the acculated value
+		for ($j = $boxRadius + 1; $j < $width - $boxRadius; $j++) {
+			$val += $sourceChannelArray[$radiusIndex++] - $sourceChannelArray[$trailingIndex++];
+			$targetChannelArray[$leadingIndex++] = round($val * $accumlatorAverager);
+		}
+
+		// finish off the row of pixels, duplicating the edge pixel instead of going out of image bounds
+		for ($j= $width - $boxRadius; $j < $width; $j++) {
+			$val += $lastValue - $sourceChannelArray[$trailingIndex++];
+			$targetChannelArray[$leadingIndex++] = round($val * $accumlatorAverager);
+		}
+	}
+
+
+}
+
+//// this does the same thing as boxBlurH, but vertically
+function boxBlurV(&$sourceChannelArray, &$targetChannelArray, $width, $height, $boxRadius) {
+	$accumlatorAverager = 1 / ($boxRadius + $boxRadius + 1);
+	for ($i=0; $i < $width; $i++) {
+		$ti = $i;
+		$li = $ti;
+		$ri = $ti + $boxRadius * $width;
+
+		$firstValue = $sourceChannelArray[$ti];
+		$lastValue = $sourceChannelArray[$ti + $width * ($height - 1)];
+		$val = ($boxRadius + 1) * $firstValue;
+
+		for ($j=0; $j < $boxRadius; $j++) {
+			$val += $sourceChannelArray[$ti + $j * $width];
+		}
+
+		for ($j=0; $j <= $boxRadius; $j++) {
+			$val += $sourceChannelArray[$ri] - $firstValue;
+			$targetChannelArray[$ti] = round($val * $accumlatorAverager);
+			$ri += $width;
+			$ti += $width;
+		}
+
+		for ($j=$boxRadius + 1; $j < $height - $boxRadius; $j++) {
+			$val += $sourceChannelArray[$ri] - $sourceChannelArray[$li];
+			$targetChannelArray[$ti] = round($val * $accumlatorAverager);
+			$li += $width;
+			$ri += $width;
+			$ti += $width;
+		}
+
+		for ($j=$height - $boxRadius; $j < $height; $j++) {
+			$val += $lastValue - $sourceChannelArray[$li];
+			$targetChannelArray[$ti] = round($val * $accumlatorAverager);
+			$li += $width;
+			$ti += $width;
 		}
 	}
 }
-
 
 ?>
