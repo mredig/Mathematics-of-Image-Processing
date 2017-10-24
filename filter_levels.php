28b98cc6a4ce9d23547dd98be374cf4a799316c4
Michael <Use Website Contact Please>
2017-10-23 23:41:02 -0500

    for diffing b

--- filter_levels.php
+++ filter_levels.php
@@ -5,7 +5,6 @@
 
 // load image
 $image1 = imagecreatefromjpeg("lighthouse.jpg");
-// $image1 = imagecreatefromjpeg("http://localhost/lighthouse.jpg");
 
 // get image dimensions for creating the final output canvas
 $dx = imagesx($image1);
@@ -22,7 +21,7 @@ imagejpeg($result);
 imagedestroy($result);
 imagedestroy($image1);
 
-function filterImage(&$imageInput, &$resultImage) {
+function filterImage(&$imageInput, &$resultImage, $blackPoint = 50, $whitePoint = 200) {
 	$dx = imagesx($imageInput);
 	$dy = imagesy($imageInput);
 
@@ -39,9 +38,24 @@ function filterImage(&$imageInput, &$resultImage) {
 			$b = $rgbColor & 0xFF;
 			imagecolordeallocate($imageInput, $rgbColor);
 
-			$rOut = $r;
-			$gOut = $g;
-			$bOut = $b;
+			// blackPoint
+			$r -= $blackPoint;
+			$r *= 255 / (255 - $blackPoint);
+			$g -= $blackPoint;
+			$g *= 255 / (255 - $blackPoint);
+			$b -= $blackPoint;
+			$b *= 255 / (255 - $blackPoint);
+
+			//whitePoint
+			$r *= 255 / $whitePoint;
+			$g *= 255 / $whitePoint;
+			$b *= 255 / $whitePoint;
+
+
+
+			$rOut = round(max(min($r, 255), 0));// make sure values are integral and do not exceed 255 or go below 0
+			$gOut = round(max(min($g, 255), 0));
+			$bOut = round(max(min($b, 255), 0));
 			//// forumla ends here!
 
 			$theColor = imagecolorallocate($resultImage, $rOut, $gOut, $bOut);
